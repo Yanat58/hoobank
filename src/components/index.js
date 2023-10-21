3ad537e221e535615a448dbb501c673ded139a23
Yanat58 <tanyalyt80@gmail.com>
2023-10-21 14:35:04 +0300

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Clients from "./Clients";
+import CTA from './CTA'
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+import NavBar from "./NavBar";
+import Business from "./Business";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+
+export {
+    NavBar,
+    Stats,
+        Business,
+        Billing,
+        CardDeal,
+        Testimonials,
+        Clients,
+        CTA,
+        Footer,
+        Hero,
+}
