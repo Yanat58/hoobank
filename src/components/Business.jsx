99cccd1f15cf36cd81e1a0a322020ff2e2108881
unknown <tanyalyt80@gmail.com>
2023-10-23 00:24:48 +0300

    fixed business

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -2,26 +2,29 @@ import { features } from '../constants';
 import styles, { layout } from '../style';
 import Button from './Button';
 
-const FeatureCard = ({ icon, title, content, index }) => (
-  <div
-    className={`flex flex-row p-6  rounded-[20px]   feature-card max-w-[500px] content-baseline items-center`}
-  >
+const FeatureCard = features => {
+  const { icon, title, content } = features;
+  return (
     <div
-      className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}
+      className={`flex flex-row p-6  rounded-[20px]   feature-card max-w-[500px] content-baseline items-center `}
     >
-      <img src={icon} alt="icon" className="w-[50%] h-[59%] object-contain" />
-    </div>
+      <div
+        className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}
+      >
+        <img src={icon} alt="icon" className="w-[50%] h-[59%] object-contain" />
+      </div>
 
-    <div className="flex-1 flex flex-col ml-3">
-      <h3 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">
-        {title}
-      </h3>
-      <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1 ">
-        {content}
-      </p>
+      <div className="flex-1 flex flex-col ml-3">
+        <h3 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">
+          {title}
+        </h3>
+        <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px] mb-1 ">
+          {content}
+        </p>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 const Business = () => {
   return (
     <section id="features" className={layout.section}>
