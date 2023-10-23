0f563ae6ff2af16cbebc5d70e4d424d99bc4f714
unknown <tanyalyt80@gmail.com>
2023-10-23 23:18:35 +0300

    add html scc in sections

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,11 +1,32 @@
-// import React from 'react'
+import { quotes } from '../assets';
 
-const FeedbackCard = () => {
+const FeedbackCard = card => {
+  const { content, name, title, img } = card;
+
   return (
-    <div>
-      FeedbackCard
-    </div>
-  )
-}
+    <div className="flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card ">
+      <img
+        src={quotes}
+        alt="double_quotes"
+        className="w-[42px] h-[42px] object-contain"
+      />
+      <p className="font-poppins font-normal text-[18px] leading-[32px] text-white my-10">
+        {content}
+      </p>
 
-export default FeedbackCard
+      <div className="flex vflex-row">
+        <img src={img} alt="name" className="w-[48px] h-[48px] rounded-full" />
+        <div className="flex flex-col ml-4">
+          <h3 className="font-poppins fomt-semibold text-[20px] leading-[32px] text-white ">
+            {name}
+          </h3>
+          <p className="font-poppins fomt-normal text-[16px] leading-[24px] text-dimWhite ">
+            {title}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeedbackCard;
